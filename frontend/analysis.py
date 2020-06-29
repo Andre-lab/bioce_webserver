d4884f3871b429617f12c172cab22ff1af5616b7
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2020-06-29 11:59:23 +0200

    Few fixes in the analysis

--- frontend/analysis.py
+++ frontend/analysis.py
@@ -40,7 +40,7 @@ def run_analysis(current_user_id):
         params = io_params.load_params(analysis_folder)
         weight_cut = analysis.weight_cut
         iterations = analysis.iterations
-        print('Params for analysis', params, weight_cut)
+        #print('Params for analysis', params, weight_cut)
         analysis_folder = params['analysis_folder']
         output_folder = params['output_folder']
         failed_folder = app.config['FAILED_FOLDER']
@@ -50,8 +50,6 @@ def run_analysis(current_user_id):
         # ----------------------------------------------------------------------
 
         try:
-            #TODO: provide access to functions here
-
             job_done = run_bioce_pipeline.run_bioce_from_webserver(params, weight_cut, iterations)
             # if we exited gracefully, just let the user know
             if not job_done:
@@ -131,6 +129,7 @@ def send_mail(email, first_name, analysis_name, subject=None, message=None):
     else:
         hi = 'Hi %s, \n\n' % first_name
     msg.body = (hi + message)
+    print("Message", message)
     mail.send(msg)
 
 
